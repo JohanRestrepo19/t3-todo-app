3c5c7ab659e0e4318a2038241f9efa5d997c4306
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-08-28 21:27:44 -0500

    feat(ui): add sidebar item components.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,26 +1,54 @@
-import { type ReactElement } from 'react'
+import { type LiHTMLAttributes } from 'react'
 import Link from 'next/link'
 import { useSession } from 'next-auth/react'
 import { ThemeToggler } from './ThemeToggler'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faHome,
+  faRectangleList,
+  faRightFromBracket,
+  faTags
+} from '@fortawesome/free-solid-svg-icons'
 
-const SidebarItem = ({
-  label,
-  path,
-  icon
-}: {
+import { type IconProp } from '@fortawesome/fontawesome-svg-core'
+
+interface Props extends LiHTMLAttributes<HTMLLIElement> {
   label: string
-  path: string
-  icon?: ReactElement
-}) => {
+  path?: string
+  icon?: IconProp
+}
+const SidebarNavItem = ({ label, path, icon, ...props }: Props) => {
   return (
-    <li>
+    <li {...props}>
       <Link
-        href={path}
+        href={path || ''}
         className="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
       >
-        {icon}
+        {icon && (
+          <FontAwesomeIcon
+            icon={icon}
+            className="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+          />
+        )}
         <span className="ml-3">{label}</span>
       </Link>
+    </li>
+  )
+}
+
+const SidebarActionItem = ({ icon, label, ...props }: Props) => {
+  return (
+    <li
+      className="group flex cursor-pointer items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+      {...props}
+    >
+      {icon && (
+        <FontAwesomeIcon
+          icon={icon}
+          className="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+        />
+      )}
+      <span className="ml-3">{label}</span>
     </li>
   )
 }
@@ -44,70 +72,19 @@ export default function Sidebar() {
           </span>
         </Link>
         <ul className="space-y-2 font-medium">
-          {/* <SidebarItem label="Home" path="/" /> */}
-          <li>
-            <a
-              href="#"
-              className="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-            >
-              <svg
-                className="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 576 512"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c.2 35.5-28.5 64.3-64 64.3H128.1c-35.3 0-64-28.7-64-64V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24zM352 224a64 64 0 1 0 -128 0 64 64 0 1 0 128 0zm-96 96c-44.2 0-80 35.8-80 80c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16c0-44.2-35.8-80-80-80H256z" />
-              </svg>
-              <span className="ml-3">Home</span>
-            </a>
-          </li>
-          <li>
-            <a className="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-              <svg
-                className="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 576 512"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M64 80c-8.8 0-16 7.2-16 16V416c0 8.8 7.2 16 16 16H512c8.8 0 16-7.2 16-16V96c0-8.8-7.2-16-16-16H64zM0 96C0 60.7 28.7 32 64 32H512c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zm96 64a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm104 0c0-13.3 10.7-24 24-24H448c13.3 0 24 10.7 24 24s-10.7 24-24 24H224c-13.3 0-24-10.7-24-24zm0 96c0-13.3 10.7-24 24-24H448c13.3 0 24 10.7 24 24s-10.7 24-24 24H224c-13.3 0-24-10.7-24-24zm0 96c0-13.3 10.7-24 24-24H448c13.3 0 24 10.7 24 24s-10.7 24-24 24H224c-13.3 0-24-10.7-24-24zm-72-64a32 32 0 1 1 0-64 32 32 0 1 1 0 64zM96 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
-              </svg>
-              <span className="ml-3 flex-1 whitespace-nowrap">All todos</span>
-            </a>
-          </li>
-          <li>
-            <Link
-              href="/categories"
-              className="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-            >
-              <svg
-                className="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 576 512"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M345 39.1L472.8 168.4c52.4 53 52.4 138.2 0 191.2L360.8 472.9c-9.3 9.4-24.5 9.5-33.9 .2s-9.5-24.5-.2-33.9L438.6 325.9c33.9-34.3 33.9-89.4 0-123.7L310.9 72.9c-9.3-9.4-9.2-24.6 .2-33.9s24.6-9.2 33.9 .2zM0 229.5V80C0 53.5 21.5 32 48 32H197.5c17 0 33.3 6.7 45.3 18.7l168 168c25 25 25 65.5 0 90.5L277.3 442.7c-25 25-65.5 25-90.5 0l-168-168C6.7 262.7 0 246.5 0 229.5zM144 144a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" />
-              </svg>
-              <span className="ml-3 flex-1 whitespace-nowrap">Categories</span>
-            </Link>
-          </li>
+          <SidebarNavItem label="Home" path="/" icon={faHome} />
+          <SidebarNavItem
+            label="All todos"
+            path="/todos"
+            icon={faRectangleList}
+          />
+          <SidebarNavItem label="Categories" path="/categories" icon={faTags} />
         </ul>
         <ul className="mt-4 space-y-2 border-t border-gray-200 pt-4 font-medium dark:border-gray-700">
           <li>
             <ThemeToggler />
           </li>
-          <li>
-            <a className="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-              <svg
-                className="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 576 512"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" />
-              </svg>
-              <span className="ml-3 flex-1 whitespace-nowrap">Logout</span>
-            </a>
-          </li>
+          <SidebarActionItem label="Logout" icon={faRightFromBracket} />
         </ul>
       </div>
     </aside>
---
+++ src/pages/todos/index.tsx
@@ -0,0 +1,7 @@
+export default function Todos() {
+  return (
+    <div>
+      <h1>Todos page</h1>
+    </div>
+  )
+}
